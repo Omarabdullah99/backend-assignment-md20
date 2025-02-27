cb51eb0bd08b8f777e047fd381748c56cd0bd3dc
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-02-27 18:44:13 +0600

    initial-setup

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser= require('cookie-parser')
+
+const app = express();
+
+//middleware
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use(cookieParser()); 
+
+//router setup
+app.get("/hello", (req, res) => {
+  res.send("hello e-commerce-project");
+});
+
+
+
+//mongodb atlas setup
+// password:khalidabdullah1147 hUm6Dh2kJb4IW7eS
+const MONGODB_URL =
+  "mongodb+srv://khalidabdullah1147:hUm6Dh2kJb4IW7eS@ostad-ass-4-mo-20.xcc66.mongodb.net/?retryWrites=true&w=majority&appName=ostad-ass-4-mo-20";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(MONGODB_URL);
+  console.log("database connected");
+}
+
+app.listen(5000,()=>{
+    console.log('port is runing 5000')
+})
